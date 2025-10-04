25332f2f27ccc7555254494d5962a6b5d8a789ac
Luis Risch <luis.risch@gmail.com>
2025-10-04 16:11:34 -0300

    fix: badge

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,8 +1,17 @@
 class Membership < ApplicationRecord
+  ROLE_TO_BADGE_COLOR_MAPPING = {
+    admin: "primary",
+    member: "secondary"
+  }.with_indifferent_access
+
   belongs_to :user
   belongs_to :group
 
   enum :role, member: 0,
               admin: 1,
               default: :member
+
+  def role_to_badge_color
+    ROLE_TO_BADGE_COLOR_MAPPING[role]
+  end
 end
